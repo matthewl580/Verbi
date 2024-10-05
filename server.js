95af598960cd7e24dbcdc770cc0c17688cea6395
matthewl580 <85137306+matthewl580@users.noreply.github.com>
2024-10-05 18:21:38 -0500

    Update and rename script.js to server.js

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+const fastify = require("fastify")({
+  // Set this to true for detailed logging:
+  logger: false,
+});
+// Setup our static files
+fastify.register(require("@fastify/static"), {
+  root: path.join(__dirname, "public"),
+  prefix: "/", // optional: default '/'
+});
+
 var dictionary = [];
-var wordList = [];
 var place = 3;
 
 setUpDictionary(true);
@@ -40,24 +49,7 @@ function fetchWord(letterNum, defNum) {
     def: Object.values(dictionary[letterNum])[defNum],
   };
 }
-function displayWord(wordObject) {
-  document.getElementById(
-    "word"
-  ).textContent = `${wordObject.word[0].toUpperCase()}${wordObject.word
-    .slice(1)
-    .toLowerCase()}`;
-  document.getElementById("def").textContent = `${wordObject.def.MEANINGS[
-    "1"
-  ][1][0].toUpperCase()}${wordObject.def.MEANINGS["1"][1]
-    .slice(1)
-    .toLowerCase()}`;
-  document.getElementById(
-    "partOfSpeech"
-  ).textContent = `(${wordObject.def.MEANINGS["1"][0]})`;
-  document.getElementById("syllables").textContent = `[${splitIntoSyllables(
-    wordObject.word
-  )}]`;
-}
+
 
 function getNextWord(direction) {
   if (direction == "forward" || direction == "f") {
@@ -145,92 +137,17 @@ function selectNewWord() {
   console.log(word);
 }
 
-// swipe functionalilty
-function detectswipe(el, func) {
-  var swipe_det = new Object();
-  swipe_det.sX = 0;
-  swipe_det.sY = 0;
-  swipe_det.eX = 0;
-  swipe_det.eY = 0;
-  var min_x = 30; //min x swipe for horizontal swipe
-  var max_x = 30; //max x difference for vertical swipe
-  var min_y = 50; //min y swipe for vertical swipe
-  var max_y = 60; //max y difference for horizontal swipe
-  var direc = "";
-  var ele = document.getElementById(el);
-  ele.addEventListener(
-    "touchstart",
-    function (e) {
-      var t = e.touches[0];
-      swipe_det.sX = t.screenX;
-      swipe_det.sY = t.screenY;
-    },
-    false
-  );
-  ele.addEventListener(
-    "touchmove",
-    function (e) {
-      e.preventDefault();
-      var t = e.touches[0];
-      swipe_det.eX = t.screenX;
-      swipe_det.eY = t.screenY;
-    },
-    false
-  );
-  ele.addEventListener(
-    "touchend",
-    function (e) {
-      //horizontal detection
-      if (
-        (swipe_det.eX - min_x > swipe_det.sX ||
-          swipe_det.eX + min_x < swipe_det.sX) &&
-        swipe_det.eY < swipe_det.sY + max_y &&
-        swipe_det.sY > swipe_det.eY - max_y &&
-        swipe_det.eX > 0
-      ) {
-        if (swipe_det.eX > swipe_det.sX) direc = "r";
-        else direc = "l";
-      }
-      //vertical detection
-      else if (
-        (swipe_det.eY - min_y > swipe_det.sY ||
-          swipe_det.eY + min_y < swipe_det.sY) &&
-        swipe_det.eX < swipe_det.sX + max_x &&
-        swipe_det.sX > swipe_det.eX - max_x &&
-        swipe_det.eY > 0
-      ) {
-        if (swipe_det.eY > swipe_det.sY) direc = "d";
-        else direc = "u";
-      }
 
-      if (direc != "") {
-        if (typeof func == "function") func(el, direc);
-      }
-      direc = "";
-      swipe_det.sX = 0;
-      swipe_det.sY = 0;
-      swipe_det.eX = 0;
-      swipe_det.eY = 0;
-    },
-    false
-  );
-}
-// debug Function
-function myfunction(el, d) {
-  alert("you swiped on element with id '" + el + "' to " + d + " direction");
-}
-detectswipe("backgroundImage", selectAndDisplayWord);
-function selectAndDisplayWord() {
-  displayWord(getNextWord("f"));
-  place++;
-}
-document.addEventListener("keyup", (event) => {
-  if (event.code === "Space" || event.code === "ArrowUp") {
-    displayWord(getNextWord("f"));
-    place++;
+
+
+// Run the server and report out to the logs
+fastify.listen(
+  { port: process.env.PORT, host: "0.0.0.0" },
+  function (err, address) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Your app is listening on ${address}`);
   }
-  if (event.code === "ArrowDown") {
-    displayWord(getNextWord("p"));
-    place--;
-  }
-});
+);
