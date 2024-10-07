939c0d4b4d2ed620efee702e9ea2bed33e2bc76f
matthewl580 <85137306+matthewl580@users.noreply.github.com>
2024-10-06 20:34:33 -0500

    Create server.js

---
+++ pages/api/server.js
@@ -0,0 +1,12 @@
+/**
+ * This is the main Node.js server script for your project
+ * Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
+ */
+
+const path = require("path");
+var fs = require('fs');
+var obj = JSON.parse(fs.readFileSync('dictionary.json', 'utf8'));
+
+export function GET(request) {
+  return new Response(obj);
+}
