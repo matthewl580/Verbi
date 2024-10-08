e17576a049ca7e48cc3c94c8781e41ae676dfd1b
matthewl580 <85137306+matthewl580@users.noreply.github.com>
2024-10-07 22:31:04 -0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -7,25 +7,8 @@
 const path = require("path");
 const fs = require('fs');
 
-function readFile(filename) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path.join(__dirname, filename), 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-}
-
-
 export function GET(request) {
-   try {
-    const data = await readFile('dictionary.json');
-    return new Response(data)
-  } catch (error) {
-    console.error('Error reading file:', error);
-     return new Response("error :(
-  }
+  let usersPath = path.join(process.cwd(), 'dictionary.json');
+  let file = fs.readFileSync(usersPath);
+  return new Response(file);
 }
