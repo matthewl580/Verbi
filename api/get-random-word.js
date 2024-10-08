27dd3b8f0456f25ce07cc26d3668af13602375b0
matthewl580 <85137306+matthewl580@users.noreply.github.com>
2024-10-07 23:41:31 -0500

    Update get-random-word.js

--- api/get-random-word.js
+++ api/get-random-word.js
@@ -9,10 +9,11 @@ const fs = require('fs');
 
 export function GET(request) {
   let usersPath = path.join(process.cwd(), 'dictionary.json');
-  let dictionary = fs.readFileSync(usersPath);
+  var dictionary = fs.readFileSync(usersPath);
   var word = { word: undefined, def: undefined };
   var i = 0;
 var wordIndex = 0;
+  console.log(dictionary)
   // skip over words that don't have a definition atached
   while (
     (word.word == undefined ||
