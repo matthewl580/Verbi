317a5c0abb7e0516bb57064429de2e764daaac9f
Shiba-2439 <matthewlozier580@gmail.com>
2024-10-19 17:48:01 -0500

    s

--- api/firebase-test.js
+++ api/firebase-test.js
@@ -1,5 +1,11 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+const firebase = require("firebase/app"); // Firebase
+const {
+  initializeApp,
+  applicationDefault,
+  cert,
+} = require("firebase-admin/app");
+
 // Initialize the FirebaseUI Widget using Firebase.
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
